770592d2d96e62220bd6d6646942f9ca5a5b474e
Alazar Kassahun <alazar.kas@gmail.com>
2025-09-22 17:04:43 +0300

    service provider update

--- src/MoneyManServiceProvider.php
+++ src/MoneyManServiceProvider.php
@@ -8,16 +8,16 @@ use Illuminate\Support\ServiceProvider;
 
 class MoneyManServiceProvider extends ServiceProvider
 {
-    public function register(): void
-    {
-        $this->mergeConfigFrom(__DIR__.'/../config/moneyman.php', 'moneyman');
-        $this->app->singleton('moneyman', fn ($app) => new MoneyManManager);
-    }
-
     public function boot(): void
     {
         $this->publishes([
             __DIR__.'/../config/moneyman.php' => config_path('moneyman.php'),
-        ]);
+        ], 'moneyman');
+    }
+
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/moneyman.php', 'moneyman');
+        $this->app->singleton('moneyman', fn ($app) => new MoneyManManager);
     }
 }
